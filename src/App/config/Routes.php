8a579207b27711ba905442a63267ac7842bb4dcb
Anouar Belhassan <135267395+BelAnouar@users.noreply.github.com>
2024-01-03 02:07:27 +0100

    M route

--- src/App/config/Routes.php
+++ src/App/config/Routes.php
@@ -13,11 +13,8 @@ use App\Controllers\HomeController;
 
 use App\Controllers\AuthController;
 use App\Controllers\LoginController;
-<<<<<<< HEAD
 use App\Controllers\SalahController;
-=======
 use App\Controllers\profilecontroller;
->>>>>>> malek
 
 function registerRoutes(App $app)
 {
@@ -25,12 +22,9 @@ function registerRoutes(App $app)
     $app->get('/', [HomeController::class, 'home']);
     $app->get('/login', [LoginController::class, 'index']);
     $app->get('/signup', [AuthController::class, 'index']);
-<<<<<<< HEAD
     $app->post('/signup', [AuthController::class, 'post']);
     $app->get('/artis', [ArtistsConrtoller::class, 'index']);
     $app->get('/yassine', [YassineController::class, 'index']);
     $app->get('/salah', [SalahController::class, 'index']);
-=======
     $app->get('/profile', [profilecontroller::class, 'index']);
->>>>>>> malek
 }
