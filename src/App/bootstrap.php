5ed11a47567f6a371eb4c14547b5afc601170924
Anouar Belhassan <135267395+BelAnouar@users.noreply.github.com>
2024-01-04 10:58:58 +0100

    change routes and bootstrap

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -7,11 +7,13 @@ require __DIR__ . "/../../vendor/autoload.php";
 use Framework\App;
 use App\Config\Paths;
 
-use function App\Config\registerMiddleware;
+
 use function App\Config\registerRoutes;
+use Dotenv\Dotenv;
 
 $app = new App(Paths::SOURCE . "app/container-definitions.php");
-
+$dotenv = Dotenv::createImmutable(Paths::ROOT);
+$dotenv->load();
 registerRoutes($app);
 
 return $app;
