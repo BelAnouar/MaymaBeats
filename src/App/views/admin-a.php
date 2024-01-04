f6e9ffb2bd7fde50274613137f52ba273b5827b0
OUMAIMAtakrour <takrouroumaima@gmail.com>
2024-01-04 18:27:43 +0900

    DASH

--- src/App/views/admin-a.php
+++ src/App/views/admin-a.php
@@ -1,164 +1,122 @@
-<?php
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/ae360af17e.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="/MaymaBeats/public/assets/adminAr.css?v=<?php echo time(); ?>">
 
-    <!-- <link rel="stylesheet" href="/MaymaBeats/public/assets/admin.css"> -->
-    <link rel="stylesheet" href="/MaymaBeats/public/assets/admin.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,700|Raleway:300,400,500,700">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
 
-    <title>Document</title>
+    <title>Artiste</title>
 </head>
 
 <body>
-
-    <section class="Dashboard">
-
-        <div class="container-fluid h-100">
-            <div class="row h-100">
-                <div id="sidebar" class="col-2">
-                    <h1>LOGO</h1>
-                    <!-- <ul class="sidelist list-unstyled mb-3 d-flex flex-column justify-content-between col-12">
-                        <img src="/MaymaBeats/public/assets/circle-user-regular.svg" alt="" class="my-5">
-                        <p class="my-3">ADMIN NAME</p>
-                        <li class="my-3 home col-12"><a href="#">HOME</a></li>
-                        <li class="my-3"><a href="#">CLIENT</a></li>
-                        <li class="my-3"><a href="#">ARTISTS</a></li>
-                        <li class="my-3"><img src="/MaymaBeats/public/assets/facebook-messenger.svg" alt=""><a href="#">NOTIFICATIONS</a></li>
-                        <li class="my-3"><a href="#">LOGOUT</a></li>
-                    </ul> -->
-                    <div class="list-group col-12 ">
-                        <img src="/MaymaBeats/public/assets/circle-user-regular.svg " alt="" class="my-4 col-8" height="20%">
-                        <a href="#" class="list-group-item list-group-item-action home">
-                            <img src="/MaymaBeats/public/assets/icons8-home-100.png" alt="" class="col-2"> HOME
+    <div class="wrapper">
+        <!-- Sidebar -->
+        <aside id="sidebar">
+            <div class="h-100">
+                <div class="sidebar-logo " style="margin-top: -9%;">
+                    <img src="vecteezy-music-writer-with-feather-and-g-clef-logo-1-658cba5074038115508023-removebg-preview.png" alt="" style="width: 65%; margin-left: -20%; ">
+                    <a href="#" style="margin-left: -15%;">MaymaBeats</a>
+                </div>
+                <!-- Sidebar Navigation -->
+                <ul class="sidebar-nav">
+                    <li class="sidebar-item">
+                        <a href="admin" class="sidebar-link">
+                            <i class="fa-solid fa-home p-2 "></i>
+                            Home
                         </a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="admin-c" class="sidebar-link">
+                            <i class="fa-solid fa-compact-disc p-2 "></i>
+                            Clients
+                        </a>
+                    </li>
 
-                        <a href="admin-c" class="list-group-item list-group-item-action bg-transparent" aria-current="true">
-                            <img src="/MaymaBeats/public/assets/icons8-clients-64.png" alt="" class="col-3">CLIENT</a>
-                        <a href="admin-a" class="list-group-item list-group-item-action bg-transparent">
-                            <img src="/MaymaBeats/public/assets/icons8-garageband-64.png" alt="" class="col-3">ARTISTS</a>
-                        <a href="#" class="list-group-item list-group-item-action bg-transparent col-12 d-flex"> <img src="/MaymaBeats/public/assets/icons8-notifications-78.png" alt="" class="col-3">NOTIFICATIONS</a>
-                        <a href="#" class="list-group-item list-group-item-action bg-transparent"><img src="/MaymaBeats/public/assets/icons8-logout-50.png" alt="" class="col-2">LOGOUT</a>
+                    <li class="sidebar-item">
+                        <a href="admin-a" class="sidebar-link">
+                            <i class="fa-regular fa-user p-2 "></i>
+                            Artists
+                        </a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="adminNot" class="sidebar-link">
+                            <i class="fa-regular fa-user p-2 "></i>
+                            Notifications
+                        </a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link">
+                            <i class="fa-solid fa-right-from-bracket p-2 "></i>
+                            Logout
+                        </a>
+                    </li>
+
+                </ul>
+            </div>
+        </aside>
+
+
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- Leave those next 4 lines if you care about users using IE8 -->
+        <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+     <![endif]-->
+
+
+
+        <div class="wrapper-grey padded">
+            <div class="container">
+                <h2 class="text-center">Your next trip</h2>
+
+                <div class="col-xs-12 col-sm-4">
+                    <div class="card" style="background: linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.2)), url('https://i0.wp.com/handluggageonly.co.uk/wp-content/uploads/2016/04/IMG_5589.jpg?w=1600&ssl=1');">
+                        <div class="card-category">City</div>
+                        <div class="card-description">
+                            <h2>home</h2>
+                            <p>Lovely house</p>
+                        </div>
+                        <img class="card-user avatar avatar-large" src="https://github.com/lewagon/bootstrap-challenges/blob/master/11-Airbnb-search-form/images/anne.jpg?raw=true">
+                        <a class="card-link" href="#"></a>
                     </div>
                 </div>
-                <div id="content" class="d-flex flex-wrap col-9 my-3 mx-5">
-                    <div id="box1">
-                        CLIENTS
-                        520
-                    </div>
-                    <div id="box2">
-                        ARTISTS
-                        135
-                    </div>
-                    <div id="extraContainer">
-                        STATISTICS
-                        <div class="additional-content">
-                            <table class="table table-hover bg-black text-white">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">#</th>
-                                        <th scope="col">First</th>
-                                        <th scope="col" class="col-2">song</th>
-                                        <th scope="col">likes</th>
-                                        <th scope="col">artist</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <th scope="row">1</th>
-                                        <td><img src="/MaymaBeats/public/assets/fire-flame-curved-solid.svg" alt="">Most liked song</td>
-                                        <td><img src="/MaymaBeats/public/assets/icons8-song-32.png" alt="" class="col-2">Otto</td>
-                                        <td>@mdo</td>
-                                        <td>@mdo</td>
-
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">2</th>
-                                        <td><img src="/MaymaBeats/public/assets/fire-flame-curved-solid.svg" alt="">Most liked album</td>
-                                        <td><img src="/MaymaBeats/public/assets/icons8-song-32.png" alt="" class="col-2">Thornton</td>
-                                        <td>@fat</td>
-                                        <td>@fat</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">3</th>
-                                        <td><img src="/MaymaBeats/public/assets/fire-flame-curved-solid.svg" alt="">Most liked lyrics</td>
-                                        <td><img src="/MaymaBeats/public/assets/icons8-song-32.png" alt="" class="col-2">@twitter</td>
-                                        <td>@twitter</td>
-                                        <td>@twitter</td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                <div class="col-xs-12 col-sm-4">
+                    <div class="card" style="background: linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.2)), url('https://i0.wp.com/handluggageonly.co.uk/wp-content/uploads/2016/03/Positano-Weather.jpg?w=1600&ssl=1');">
+                        <div class="card-category">City</div>
+                        <div class="card-description">
+                            <h2>Home</h2>
+                            <p>Lovely house</p>
                         </div>
+                        <img class="card-user avatar avatar-large" src="https://github.com/lewagon/bootstrap-challenges/blob/master/11-Airbnb-search-form/images/anne.jpg?raw=true">
+                        <a class="card-link" href="#"></a>
                     </div>
-                    <div id="box3">
-                        <div class="d-flex flex-column">
-                            MOST ACTIVE ARTIST
-
-                            <div class="progress my-3" role="progressbar" aria-label="Success example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-                                <img src="/MaymaBeats/public/assets/icons8-disk.gif" alt="" class="col-1">
-                                <div class="progress-bar bg-success" style="width: 25%"></div>
-                            </div>
-                            <div class="progress my-3" role="progressbar" aria-label="Info example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
-                                <img src="/MaymaBeats/public/assets/icons8-disk.gif" alt="" class="col-1">
-                                <div class="progress-bar bg-info" style="width: 50%"></div>
-                            </div>
-                            <div class="progress my-3" role="progressbar" aria-label="Warning example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-                                <img src="/MaymaBeats/public/assets/icons8-disk.gif" alt="" class="col-1">
-                                <div class="progress-bar bg-warning" style="width: 75%"></div>
-                            </div>
-                            <div class="progress my-3" role="progressbar" aria-label="Danger example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
-                                <img src="/MaymaBeats/public/assets/icons8-disk.gif" alt="" class="col-1">
-                                <div class="progress-bar bg-danger" style="width: 100%"></div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div id="box4">
-                        <div class="d-flex flex-column">
-                            RECLAMATIONS
-                            <table class="table">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">#</th>
-                                        <th scope="col">Lyrics</th>
-                                        <th scope="col">song</th>
-                                        <th scope="col">Numbers</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <th scope="row">1</th>
-                                        <td>Mark</td>
-                                        <td>lately</td>
-                                        <td>220</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">2</th>
-                                        <td>Jacob</td>
-                                        <td>Thornton</td>
-                                        <td>76</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">3</th>
-                                        <td colspan="2">Larry the Bird</td>
-                                        <td>61</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-
                 </div>
             </div>
         </div>
-    </section>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    </div>
+
+    <!-- Including Bootstrap JS (with its jQuery dependency) so that dynamic components work -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <script>
+        const toggler = document.querySelector(".btn");
+        toggler.addEventListener("click", function() {
+            document.querySelector("#sidebar").classList.toggle("collapsed");
+        });
+    </script>
 
 </body>
 
